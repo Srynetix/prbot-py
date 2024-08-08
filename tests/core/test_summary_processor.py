c55fc974e886783adf0a4ad0c7fc603a07ed2d4e
Denis BOURGE <denis.bourge@sharingcloud.com>
2024-08-08 08:44:45 +0000

    fix: ensure summary will only be created once per PR

--- tests/core/test_summary_processor.py
+++ tests/core/test_summary_processor.py
@@ -1,5 +1,3 @@
-import asyncio
-
 import pytest
 
 from prbot.core.models import PullRequest, Repository
@@ -79,44 +77,3 @@ async def test_summary_update() -> None:
 
     processor = SummaryProcessor()
     await processor.process(sync_state=dummy_sync_state())
-
-
-async def test_summary_generation_parallel() -> None:
-    fake_github = get_fake_github_http_client()
-    fake_lock = get_fake_lock_client()
-
-    # Setup database
-    repository_db = inject_instance(RepositoryDatabase)
-    pull_request_db = inject_instance(PullRequestDatabase)
-    repository = await repository_db.create(Repository(owner="owner", name="name"))
-    await pull_request_db.create(
-        PullRequest(number=1, repository_path=repository.path())
-    )
-
-    fake_github.expect(
-        HttpExpectation()
-        .with_times(1)
-        .with_input(
-            method="POST",
-            url="/repos/owner/name/issues/1/comments",
-            json=HttpExpectation.IGNORE,
-        )
-        .with_output_status(200)
-        .with_output_model(GhCommentResponse(id=1))
-    )
-
-    fake_lock.expect(
-        LockExpectation()
-        .with_times(10)
-        .with_input_action("lock")
-        .with_output_function_as_once_lock()
-    )
-
-    async def inner() -> None:
-        processor = SummaryProcessor()
-        await processor.process(sync_state=dummy_sync_state(status_comment_id=0))
-
-    # Run everything in parallel, fingers crossed.
-    await asyncio.gather(*[inner() for _ in range(10)])
-
-    # If everything is alright, no exceptions, because API will only be called once.
